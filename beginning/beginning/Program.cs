781155a39b2131731d374ff3b9dc6c217d33ac21
Jan Throm <jan.throm@tecalliance.net>
2022-09-15 10:58:47 +0200

    tested all the loops #1

--- beginning/beginning/Program.cs
+++ beginning/beginning/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace beginning
@@ -11,13 +12,38 @@ namespace beginning
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Gib mit ein Wort/satz");
-            string entry = Console.ReadLine();
 
-            string subEntry = entry.Substring(3,1);
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine(i);
+            }
 
-            Console.WriteLine("old: " + entry + ":");
-            Console.WriteLine("new: " + subEntry + ":");
+            Console.WriteLine("------------------------------------------");
+
+            int j = 0; 
+
+            while (j < 7)
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+
+            Console.WriteLine("------------------------------------------");
+
+            int[] intArray = new []{ 1,2,3};
+            foreach (var k in intArray)
+            {
+                Console.WriteLine(k);
+            }
+
+            Console.WriteLine("------------------------------------------");
+
+            int l = 0;
+            do
+            {
+                Console.WriteLine(l);
+                l++;
+            } while (l<15);
 
             Console.ReadKey();
         }
