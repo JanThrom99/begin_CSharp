fea6a2fedbf618a9605d61a33f05e123e5bf9460
Jan Throm <jan.throm@tecalliance.net>
2022-09-15 09:51:38 +0200

    removed old code and added some number stuff #1

--- beginning/beginning/Program.cs
+++ beginning/beginning/Program.cs
@@ -10,14 +10,10 @@ namespace beginning
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("bitte gib deinen Namen ein");
-            string userName = Console.ReadLine();
-
-            Console.WriteLine("wie alt bist du (in Jahren)");
-            int age = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("hallo "+userName + " wieso um himmels willen bist du " + age + " Jahre alt?!");
-            Console.ReadKey();
+            double height = 1.76;
+            int multiplicator = 3; 
+            Console.WriteLine("du bist " + multiplicator*height + " groß");
+            Console.ReadLine();
         }
     }
 }
