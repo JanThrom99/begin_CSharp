cf4d2e5f4c0dd563a372ca8bc7beb6677a64a862
Jan Throm <jan.throm@tecalliance.net>
2022-09-15 09:05:06 +0200

    initial Project creation #1

---
+++ beginning/beginning/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beginning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Krösus");
+            Console.ReadKey();
+        }
+    }
+}
